093dd5b2094238234377195badb86693b763e514
jungjael <ljjae321@gmail.com>
2016-11-10 15:07:50 +0900

    일단 레이아웃 부분 수정및 추가

--- A.T.T/src/main/java/com/gmail/lkj161226/HomeController.java
+++ A.T.T/src/main/java/com/gmail/lkj161226/HomeController.java
@@ -14,30 +14,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
-public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	
-	/**
-	 * ¸íÈÆÀÌµµ Â¯Â¯¸Ç!!!!!!!
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+
+	@Controller
+	public class HomeController {
+
+		private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		@RequestMapping(value = "/", method = RequestMethod.GET)
+		public String home(Locale locale, Model model) {
+			return "home";
+		}
+		@RequestMapping(value = "/main", method = RequestMethod.GET)
+		public String main(Locale locale, Model model) {
+			return "home";
+		}
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
 	}
-	
-}
